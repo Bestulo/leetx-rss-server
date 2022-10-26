0b6abfc50b5a603fb6c3aff4d93379ed019a1250
Besto <manofthebag@gmail.com>
2022-10-26 17:53:55 +0200

    first commit

---
+++ leetResultToXml.ts
@@ -0,0 +1,73 @@
+import { TorrentDetails } from "https://deno.land/x/leetx@v1.0.0/types/TorrentDetails.ts";
+
+const searchedString = "The Matrix";
+
+// const torrents = await fullSearch(searchedString);
+
+export default function leetResultToXml(leetResult: TorrentDetails[]) {
+  const time = leetResult[0].time;
+
+  // Dec. 22nd '21 to date, grab month, day and year
+  function parseDate(date: string) {
+    const [month, day, year] = date
+      .replace(",", "")
+      .replace("'", "")
+      .replace(".", "")
+      .split(" ");
+
+    // remove letters from day
+    const dayNumber = day.replace(/\D/g, "");
+    // original: Dec. 22nd '21
+    // parsedString: 22/Dec/21
+    // date: 2021-12-21T23:00:00.000Z
+    return new Date(`${dayNumber}/${month}/${year}`);
+  }
+
+  // console.log(torrents.map((torrent) => torrent.name));
+
+  const xmlStart = `<?xml version="1.0" encoding="UTF-8"?>
+	<feed xmlns="http://www.w3.org/2005/Atom">
+		<title>1337x RSS - ${searchedString}</title>
+		<id>https://besto.me/</id>
+		<link rel="alternate" href="https://besto.me/about-me/"/>
+		<link href="https://gist.githubusercontent.com/Bestulo/a3813880eeb77513360ed23a7a2c3950/raw/30622c8b7064fafc6668902fb7930a4c039a0409/1337x-matrix-rss.xml" rel="self"/>
+	
+		<updated>${new Date().toISOString()}</updated>
+		<author>
+			<name>Besto's Magic 1337x RSS Hub</name>
+		</author>`;
+
+  const xmlEnd = `</feed>
+	`;
+
+  const torrentRSSFeed = leetResult.map((torrent) => {
+    const xmlFeed = `		<entry>
+				<title>${torrent.name}</title>
+				<link rel="alternate" type="text/html" href="${torrent.url}"/>
+				<id>${torrent.url}</id>
+				<published>${parseDate(time).toISOString()}</published>
+				<updated>${parseDate(time).toISOString()}</updated>
+				<category term="${torrent.category}"/>
+				<content type="html"><![CDATA[${torrent.description?.replaceAll(
+          "\n",
+          ""
+        )}]]></content>
+			</entry>`;
+    return xmlFeed;
+  });
+  return new TextEncoder().encode(
+    [xmlStart, ...torrentRSSFeed, xmlEnd].join("\n")
+  );
+}
+
+// Deno.writeFileSync(
+//   "torrents.xml",
+//   new TextEncoder().encode([xmlStart, ...torrentRSSFeed, xmlEnd].join("\n"))
+// );
+
+/*
+Known errors:
+Self reference doesn't match document location -- can't solve until self hosting
+Two entries with the same value for atom:updated -- can't fix cuz 1337x doesn't have precision of over a day for dates
+Not stored as XML file -- can't fix until self hosting to set MIME type
+*/
---
+++ server.ts
@@ -0,0 +1,27 @@
+import fullSearch from "https://deno.land/x/leetx@v1.0.0/mod.ts";
+import { Application } from "https://deno.land/x/oak@v10.6.0/mod.ts";
+import leetResultToXml from "./leetResultToXml.ts";
+
+const app = new Application();
+
+app.use(async (ctx) => {
+  // url will be like this: example.com/search?query=the+matrix
+  const searchedString = ctx.request.url.searchParams.get("query");
+  if (!searchedString) {
+    ctx.response.body =
+      "No search query provided; please add ?query=your+search+query to the url";
+    // add a 400 status code
+    ctx.response.status = 400;
+    return;
+  }
+  // search it
+  const torrentDetails = await fullSearch(searchedString);
+  // convert it to xml
+  const xml = leetResultToXml(torrentDetails);
+  // set the content type to application/rss+xml
+  ctx.response.headers.set("Content-Type", "application/rss+xml");
+  // send the xml
+  ctx.response.body = xml;
+});
+
+await app.listen({ port: 8000 });
